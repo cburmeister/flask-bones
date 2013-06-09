234897a36cdf5a5cf5b7550f6d176f4168d7a6c7
Corey Burmeister <burmeister.corey@gmail.com>
2013-06-09 11:21:45 -0700

    add basic test suite

---
+++ tests.py
@@ -0,0 +1,20 @@
+import os
+import app
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.
+        self.app = app.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_index(self):
+        resp = self.app.get('/')
+        assert 'Hello World!' in resp.data
+
+if __name__ == '__main__':
+    unittest.main()
