d971235d732b52e53a75a49341670d25bb4f9378
Corey Burmeister <burmeister.corey@gmail.com>
2013-07-21 22:13:41 -0700

    add request_time to g

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, request, g
+import time
 
 app = Flask(__name__)
 
 from app import views
+
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: '%.5fs' % (time.time() - g.request_start_time)
