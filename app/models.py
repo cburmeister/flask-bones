6fe2d357d472d25445758b54adac208445882649
Corey Burmeister <burmeister.corey@gmail.com>
2013-09-14 14:09:44 -0700

    setup postgres, sqlalchemy and heroku

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
