77c07331dface1237d4941c223dd8b6f63367043
Corey Burmeister <burmeister.corey@gmail.com>
2013-09-22 12:55:49 -0700

    fixes some markup

--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,5 @@ from flask import flash
 def flash_errors(form, category='danger'):
     for field, errors in form.errors.items():
         for error in errors:
-            flash(u'%s' % (error), category)
+            flash(u'%s - %s' % (getattr(form, field).label.text,
+                error), category)
