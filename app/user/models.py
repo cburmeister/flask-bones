2ae308b4e65a8f485ec68de8aa5d90dcc2253056
Corey Burmeister <burmeister.corey@gmail.com>
2017-11-11 14:08:51 -0800

    Slim down app.user.models.User.__init__

--- app/user/models.py
+++ app/user/models.py
@@ -1,7 +1,9 @@
-from flask_login import UserMixin
-from app.extensions import cache, bcrypt
-from app.database import db, CRUDMixin
 import datetime
+
+from flask_login import UserMixin
+
+from app.database import db, CRUDMixin
+from app.extensions import bcrypt
 
 
 class User(CRUDMixin, UserMixin, db.Model):
@@ -9,22 +11,21 @@ class User(CRUDMixin, UserMixin, db.Model):
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(128), nullable=False, unique=True)
     pw_hash = db.Column(db.String(60), nullable=False)
-    created_ts = db.Column(db.DateTime(timezone=True),
-            server_default=db.func.current_timestamp(),)
-    updated_ts = db.Column(db.DateTime(timezone=True),
-            onupdate=db.func.current_timestamp(),)
+    created_ts = db.Column(
+        db.DateTime(timezone=True),
+        default=datetime.datetime.utcnow
+    )
+    updated_ts = db.Column(
+        db.DateTime(timezone=True),
+        default=datetime.datetime.utcnow
+    )
     remote_addr = db.Column(db.String(20))
     active = db.Column(db.Boolean())
     is_admin = db.Column(db.Boolean())
 
-    def __init__(self, username, email, password, remote_addr, active=False, is_admin=False):
-        self.username = username
-        self.email = email
+    def __init__(self, password, **kwargs):
+        super(User, self).__init__(**kwargs)
         self.set_password(password)
-        self.created_ts = datetime.datetime.now()
-        self.remote_addr = remote_addr
-        self.active = active
-        self.is_admin = is_admin
 
     def __repr__(self):
         return '<User #%s:%r>' % (self.id, self.username)
