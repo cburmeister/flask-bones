625a636904f78cc54a487b0c2d0688ff1db853b2
Stewart Adam <s.adam@diffingo.com>
2018-05-11 13:10:42 -0700

    Add Flask-Limiter extension for rate-limiting routes or API methods

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,8 @@
 from flask_assets import Environment
 from flask_babel import Babel
 from flask_bcrypt import Bcrypt
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -12,6 +14,7 @@ assets = Environment()
 babel = Babel()
 bcrypt = Bcrypt()
 cache = SimpleCache()
+limiter = Limiter(key_func=get_remote_address)
 lm = LoginManager()
 mail = Mail()
 migrate = Migrate()
